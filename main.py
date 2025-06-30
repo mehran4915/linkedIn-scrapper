2065b13cafa33ee74bafbfcd3f8b4035e74e9db9
mehran <mehan4915@gmail.com>
2025-06-30 13:43:47 +0330

    basic scraping for job and location

--- main.py
+++ main.py
@@ -9,16 +9,9 @@ wanted_location = input("Enter location that you want: ")
 
 driver = webdriver.Firefox()
 
-driver.get(f"https://www.linkedin.com/jobs/search?keywords={wanted_job}&position=1&pagenum=0")
+driver.get(f"https://www.linkedin.com/jobs/search?keywords={wanted_job}&location={wanted_location}&position=1&pagenum=0")
 
 time.sleep(5)
-
-location_search_bar = driver.find_element(By.ID, "job-search-bar-location")
-location_search_bar.clear()
-location_search_bar.send_keys(wanted_location)
-submit_location = driver.find_element(By.CLASS_NAME, 'base-search-bar__submit-btn')
-submit_location.click()
-
 
 forum = driver.find_element(By.CLASS_NAME, 'jobs-search__results-list')
 jobs_listed = forum.find_elements(By.TAG_NAME, 'li')
@@ -27,5 +20,6 @@ for li in jobs_listed:
     title = li.find_element(By.CLASS_NAME, 'base-search-card__title').text
     link = li.find_element(By.TAG_NAME, 'a').get_attribute("href")
     location = li.find_element(By.CLASS_NAME , 'job-search-card__location').text
+    listed_time = li.find_element(By.CLASS_NAME, "job-search-card__listdate").text
 
-    print(f"Title: {title}, Location: {location}\n link: {link}\n")
+    print(f"Title: {title}, LOCATION: {location}\n, TIME:{listed_time}  , link: {link}\n")
